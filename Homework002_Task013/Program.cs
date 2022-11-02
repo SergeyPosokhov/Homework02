473f07659ee46d0a1cc816f4b8ae2b3639936c8c
Sergey Posokhov <posokhov_pit@mail.ru>
2022-11-02 12:20:48 +0300

    init: created Homework002 Task 010 and 013

---
+++ Homework002_Task013/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите какое-нибудь число ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num <= 99)
+{
+    Console.WriteLine("Третьей цифры нет ");
+}
+else
+{
+    int i = 10;
+    while (num > 999)
+    {
+        num = num / i;
+    }
+Console.WriteLine(num % 10);    
+}
